c23a8c2a928afc8f20db684dd0c11e971efe91c6
min <48940875+Evelyn0116@users.noreply.github.com>
2020-09-24 21:57:02 -0700

    Initial commit

---
+++ app/src/main/java/com/example/ecommerce/AdminCategoryActivity.java
@@ -0,0 +1,60 @@
+package com.example.ecommerce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class AdminCategoryActivity extends AppCompatActivity {
+
+    private ImageView cloth, pants, bag, shoes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_category);
+
+        cloth = findViewById(R.id.cloth);
+        pants = findViewById(R.id.pants);
+        bag = findViewById(R.id.bag);
+        shoes = findViewById(R.id.shoes);
+
+        cloth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "Cloths");
+                startActivity(intent);
+            }
+        });
+
+        pants.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "Pants");
+                startActivity(intent);
+            }
+        });
+
+        bag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "Bag");
+                startActivity(intent);
+            }
+        });
+
+        shoes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "shoes");
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/ecommerce/Prevalen/Prevalent.java
@@ -0,0 +1,11 @@
+package com.example.ecommerce.Prevalen;
+
+import com.example.ecommerce.Model.Users;
+
+//memory current user status -"remember me"
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
